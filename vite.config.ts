5436acc7f2ae7ecde904481dd56bd5c50c2c941c
Eric Zhang <ekzhang1@gmail.com>
2025-04-29 23:31:52 -0400

    Fix proxy

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,15 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+
+  server: {
+    proxy: {
+      // Set up socket.io proxy to backend in development.
+      "/socket.io": {
+        target: "http://localhost:3000",
+        ws: true,
+        changeOrigin: true,
+      },
+    },
+  },
 });
